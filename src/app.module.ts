e2eacf9edcca88c4898422cec115d24faa17b202
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-05-14 11:16:59 +0200

    added docker configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), PrismaModule],
 })
 export class AppModule {}
