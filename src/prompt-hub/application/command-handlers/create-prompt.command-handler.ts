fd13b98b83bc10fa0390732cd4d8b2ec17601c8a
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-05-15 08:07:25 +0200

    WIP

--- src/prompt-hub/application/command-handlers/create-prompt.command-handler.ts
+++ src/prompt-hub/application/command-handlers/create-prompt.command-handler.ts
@@ -1,10 +1,16 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs';
 import { CreatePromptCommand } from '../commands';
+import { PromptRepository } from '../ports';
 
 @CommandHandler(CreatePromptCommand)
 export class CreatePromptCommandHandler
   implements ICommandHandler<CreatePromptCommand, void>
 {
+  constructor(
+    private readonly promptRepository: PromptRepository,
+    private readonly eventPublisher: EventPublisher,
+  ) {}
+
   execute(command: CreatePromptCommand): Promise<void> {
     throw new Error('Method not implemented.');
   }
