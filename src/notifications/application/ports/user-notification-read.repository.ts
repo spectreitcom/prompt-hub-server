334354bedf6c9e8fad77c6ca63203e2800229c8e
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-05-16 14:39:57 +0200

    WIP

---
+++ src/notifications/application/ports/user-notification-read.repository.ts
@@ -0,0 +1,10 @@
+import { UserId } from '../../domain';
+import { NotificationView } from '../../views';
+
+export abstract class UserNotificationReadRepository {
+  abstract findForUser(
+    id: UserId,
+    take: number,
+    skip: number,
+  ): Promise<NotificationView[]>;
+}
