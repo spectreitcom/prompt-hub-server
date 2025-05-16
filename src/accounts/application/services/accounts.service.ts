650caa2598112e7b9f104526055f58e62c9feb2e
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-05-16 13:23:21 +0200

    WIP

--- src/accounts/application/services/accounts.service.ts
+++ src/accounts/application/services/accounts.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { CommandBus } from '@nestjs/cqrs';
+import { SignUpWithGmailCommand } from '../commands';
 
 @Injectable()
-export class AccountsService {}
+export class AccountsService {
+  constructor(private readonly commandBus: CommandBus) {}
+
+  async signUpWithGmail(
+    googleId: string,
+    email: string,
+    name: string,
+    avatarUrl?: string,
+  ): Promise<void> {
+    const command = new SignUpWithGmailCommand(
+      googleId,
+      email,
+      name,
+      avatarUrl,
+    );
+
+    await this.commandBus.execute(command);
+  }
+}
