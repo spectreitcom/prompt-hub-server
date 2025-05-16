47321b2f3b92c8af9631602ac966330f675d6f2f
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-05-16 13:25:32 +0200

    WIP

--- src/accounts/application/services/accounts.service.ts
+++ src/accounts/application/services/accounts.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { SignUpWithGmailCommand } from '../commands';
+import { UserRepository } from '../ports';
+import { EmailAddress } from '../../domain';
 
 @Injectable()
 export class AccountsService {
-  constructor(private readonly commandBus: CommandBus) {}
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly userRepository: UserRepository,
+  ) {}
 
+  /**
+   * Registers a new user using their Gmail account information.
+   *
+   * @param {string} googleId - The unique identifier associated with the user's Google account.
+   * @param {string} email - The email address associated with the user's Google account.
+   * @param {string} name - The name of the user as provided in their Google account.
+   * @param {string} [avatarUrl] - The optional URL to the user's profile picture from their Google account.
+   * @return {Promise<void>} A promise that resolves when the sign-up process is completed.
+   */
   async signUpWithGmail(
     googleId: string,
     email: string,
@@ -20,5 +34,15 @@ export class AccountsService {
     );
 
     await this.commandBus.execute(command);
+  }
+
+  /**
+   * Finds a user by their email address.
+   *
+   * @param {string} email - The email address of the user to find.
+   * @return {Promise<Object|null>} A promise that resolves to the user object if found, or null if no user is found.
+   */
+  async findByEmail(email: string) {
+    return this.userRepository.findByEmail(EmailAddress.create(email));
   }
 }
