79d16cb9da9ff298c295043ba3b50b0e95c8d642
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-05-17 14:27:35 +0200

    WIP

--- src/prompt-hub/application/event-handlers/prompt-copied.event-handler.ts
+++ src/prompt-hub/application/event-handlers/prompt-copied.event-handler.ts
@@ -1,16 +1,70 @@
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
-import { PromptCopiedEvent } from '../../domain';
-import { Logger } from '@nestjs/common';
+import { PromptCopiedEvent, PromptId } from '../../domain';
+import {
+  PromptDetailsViewRepository,
+  PromptListItemViewRepository,
+} from '../ports';
+import { PromptDetailsView, PromptListItemView } from '../../views';
 
 @EventsHandler(PromptCopiedEvent)
 export class PromptCopiedEventHandler
   implements IEventHandler<PromptCopiedEvent>
 {
-  private readonly logger = new Logger(PromptCopiedEventHandler.name);
+  constructor(
+    private readonly promptListItemViewRepository: PromptListItemViewRepository,
+    private readonly promptDetailsViewRepository: PromptDetailsViewRepository,
+  ) {}
 
-  handle(event: PromptCopiedEvent) {
-    this.logger.debug(
-      `Prompt ${event.promptId.getValue()} was copied by user ${event.byUserId.getValue()}`,
+  async handle(event: PromptCopiedEvent) {
+    const { promptId } = event;
+
+    await this.updatePromptListItemView(promptId);
+    await this.updatePromptDetailsView(promptId);
+  }
+
+  private async updatePromptListItemView(promptId: PromptId) {
+    // prompt list item view
+    const promptListItemView = await this.promptListItemViewRepository.findById(
+      promptId.getValue(),
     );
+
+    if (!promptListItemView) return;
+
+    const promptListItemViewToUpdate = new PromptListItemView(
+      promptListItemView.id,
+      promptListItemView.title,
+      promptListItemView.contentPreview,
+      promptListItemView.likedCount,
+      promptListItemView.copiedCount + 1,
+      promptListItemView.viewCount,
+      promptListItemView.createdAt,
+      promptListItemView.author,
+    );
+
+    await this.promptListItemViewRepository.save(promptListItemViewToUpdate);
+  }
+
+  private async updatePromptDetailsView(promptId: PromptId) {
+    // prompt details view
+    const promptDetailsView = await this.promptDetailsViewRepository.findById(
+      promptId.getValue(),
+    );
+
+    if (!promptDetailsView) return;
+
+    const promptDetailsViewToUpdate = new PromptDetailsView(
+      promptDetailsView.id,
+      promptDetailsView.title,
+      promptDetailsView.content,
+      promptDetailsView.isPublic,
+      promptDetailsView.status,
+      promptDetailsView.createdAt,
+      promptDetailsView.likedCount,
+      promptDetailsView.copiedCount + 1,
+      promptDetailsView.viewCount,
+      promptDetailsView.author,
+    );
+
+    await this.promptDetailsViewRepository.save(promptDetailsViewToUpdate);
   }
 }
