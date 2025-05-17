518270c18e1fa10ffa87ca058f538501173474e9
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-05-17 12:17:35 +0200

    WIP

--- src/prompt-hub/application/ports/prompt-list-item-view.repository.ts
+++ src/prompt-hub/application/ports/prompt-list-item-view.repository.ts
@@ -2,4 +2,9 @@ import { PromptListItemView } from '../../views';
 
 export abstract class PromptListItemViewRepository {
   abstract save(promptListItemView: PromptListItemView): Promise<void>;
+  abstract getList(
+    take: number,
+    skip: number,
+    search?: string,
+  ): Promise<PromptListItemView[]>;
 }
