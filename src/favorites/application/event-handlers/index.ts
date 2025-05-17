8751757675ba7388a0ae1deb56a0f2073c166c50
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-05-17 10:33:07 +0200

    WIP

--- src/favorites/application/event-handlers/index.ts
+++ src/favorites/application/event-handlers/index.ts
@@ -1,10 +1,13 @@
 import { FavoritePromptCreatedEventHandler } from './favorite-prompt-created.event-handler';
 import { FavoritePromptRemovedEventHandler } from './favorite-prompt-removed.event-handler';
+import { UserCreatedEventHandler } from './user-created.event-handler';
 
 export const eventHandlers = [
   FavoritePromptCreatedEventHandler,
   FavoritePromptRemovedEventHandler,
+  UserCreatedEventHandler,
 ];
 
 export * from './favorite-prompt-created.event-handler';
 export * from './favorite-prompt-removed.event-handler';
+export * from './user-created.event-handler';
