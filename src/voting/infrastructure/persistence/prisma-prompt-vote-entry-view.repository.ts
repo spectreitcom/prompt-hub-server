226d2355d865c8081ef76b646a85834bcde1797b
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-05-18 09:58:53 +0200

    WIP

--- src/voting/infrastructure/persistence/prisma-prompt-vote-entry-view.repository.ts
+++ src/voting/infrastructure/persistence/prisma-prompt-vote-entry-view.repository.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../../prisma';
 import { PromptVoteEntryViewRepository } from '../../application';
 import { PromptVoteEntryView } from '../../views';
 
@@ -6,18 +7,62 @@ import { PromptVoteEntryView } from '../../views';
 export class PrismaPromptVoteEntryViewRepository
   implements PromptVoteEntryViewRepository
 {
-  exists(promptId: string, userId: string): Promise<boolean> {
-    return Promise.resolve(false);
+  constructor(private readonly prisma: PrismaService) {}
+
+  async exists(promptId: string, userId: string): Promise<boolean> {
+    const count = await this.prisma.promptVoteEntry.count({
+      where: {
+        promptId,
+        userId,
+      },
+    });
+
+    return count > 0;
   }
 
-  findByPromptAndUser(
+  async findByPromptAndUser(
     promptId: string,
     userId: string,
   ): Promise<PromptVoteEntryView> {
-    return Promise.resolve(undefined);
+    const entry = await this.prisma.promptVoteEntry.findUnique({
+      where: {
+        userId_promptId: {
+          userId,
+          promptId,
+        },
+      },
+    });
+
+    if (!entry) {
+      return null;
+    }
+
+    return new PromptVoteEntryView(
+      entry.userId,
+      entry.promptId,
+      entry.vote,
+      entry.votedAt,
+    );
   }
 
-  save(promptVoteEntryView: PromptVoteEntryView): Promise<void> {
-    return Promise.resolve(undefined);
+  async save(promptVoteEntryView: PromptVoteEntryView): Promise<void> {
+    await this.prisma.promptVoteEntry.upsert({
+      where: {
+        userId_promptId: {
+          userId: promptVoteEntryView.userId,
+          promptId: promptVoteEntryView.promptId,
+        },
+      },
+      update: {
+        vote: promptVoteEntryView.vote,
+        votedAt: promptVoteEntryView.votedAt,
+      },
+      create: {
+        userId: promptVoteEntryView.userId,
+        promptId: promptVoteEntryView.promptId,
+        vote: promptVoteEntryView.vote,
+        votedAt: promptVoteEntryView.votedAt,
+      },
+    });
   }
 }
