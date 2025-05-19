cb91dacd74249825651f0762159d86356e478b3e
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-05-19 10:49:55 +0200

    WIP

---
+++ src/prompt-hub/application/event-handlers/prompt-viewed.event-handler.ts
@@ -0,0 +1,82 @@
+import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
+import { PromptViewedEvent, PromptId, UserId } from '../../domain';
+import {
+  PromptDetailsViewRepository,
+  PromptListItemViewRepository,
+} from '../ports';
+import { PromptDetailsView, PromptListItemView } from '../../views';
+
+@EventsHandler(PromptViewedEvent)
+export class PromptViewedEventHandler
+  implements IEventHandler<PromptViewedEvent>
+{
+  constructor(
+    private readonly promptListItemViewRepository: PromptListItemViewRepository,
+    private readonly promptDetailsViewRepository: PromptDetailsViewRepository,
+  ) {}
+
+  async handle(event: PromptViewedEvent) {
+    const { promptId, byUserId } = event;
+
+    await this.updatePromptListItemView(promptId, byUserId);
+    await this.updatePromptDetailsView(promptId, byUserId);
+  }
+
+  private async updatePromptListItemView(promptId: PromptId, byUserId: UserId) {
+    // prompt list item view
+    const promptListItemView = await this.promptListItemViewRepository.findById(
+      promptId.getValue(),
+    );
+
+    if (!promptListItemView) return;
+
+    // Don't increment viewCount if the prompt is viewed by its owner
+    const isOwner = promptListItemView.author.id === byUserId.getValue();
+    const newViewCount = isOwner
+      ? promptListItemView.viewCount
+      : promptListItemView.viewCount + 1;
+
+    const promptListItemViewToUpdate = new PromptListItemView(
+      promptListItemView.id,
+      promptListItemView.title,
+      promptListItemView.contentPreview,
+      promptListItemView.likedCount,
+      promptListItemView.copiedCount,
+      newViewCount,
+      promptListItemView.createdAt,
+      promptListItemView.author,
+    );
+
+    await this.promptListItemViewRepository.save(promptListItemViewToUpdate);
+  }
+
+  private async updatePromptDetailsView(promptId: PromptId, byUserId: UserId) {
+    // prompt details view
+    const promptDetailsView = await this.promptDetailsViewRepository.findById(
+      promptId.getValue(),
+    );
+
+    if (!promptDetailsView) return;
+
+    // Don't increment viewCount if the prompt is viewed by its owner
+    const isOwner = promptDetailsView.author.id === byUserId.getValue();
+    const newViewCount = isOwner
+      ? promptDetailsView.viewCount
+      : promptDetailsView.viewCount + 1;
+
+    const promptDetailsViewToUpdate = new PromptDetailsView(
+      promptDetailsView.id,
+      promptDetailsView.title,
+      promptDetailsView.content,
+      promptDetailsView.isPublic,
+      promptDetailsView.status,
+      promptDetailsView.createdAt,
+      promptDetailsView.likedCount,
+      promptDetailsView.copiedCount,
+      newViewCount,
+      promptDetailsView.author,
+    );
+
+    await this.promptDetailsViewRepository.save(promptDetailsViewToUpdate);
+  }
+}
