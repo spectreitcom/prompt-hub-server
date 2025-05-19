0fb80303f5b1e57ed10f756d9aa897b7ea124f1e
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-05-19 10:15:18 +0200

    WIP

--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { InfrastructureModule } from './infrastructure';
 import { CqrsModule } from '@nestjs/cqrs';
 import * as EventHandlers from './application/event-handlers';
+import { InfrastructureModule as AccountsInfrastructureModule } from '../accounts/infrastructure';
 
 @Module({
-  imports: [InfrastructureModule, CqrsModule],
+  imports: [InfrastructureModule, CqrsModule, AccountsInfrastructureModule],
   providers: [...Object.values(EventHandlers)],
   exports: [],
 })
