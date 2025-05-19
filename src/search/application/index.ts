4968d484b4b63ed44ff18b6dc3173ad7f28cba1f
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-05-19 08:39:52 +0200

    WIP

--- src/search/application/index.ts
+++ src/search/application/index.ts
@@ -2,3 +2,5 @@ export * from './command-handlers';
 export * from './commands';
 export * from './event-handlers';
 export * from './ports';
+export * from './queries';
+export * from './query-handlers';
