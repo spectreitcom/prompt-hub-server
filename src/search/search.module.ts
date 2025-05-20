8b4c5f2e73cc674b5d833531ec8e3735c7b1ddce
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-05-20 14:18:20 +0200

    added a new endpoint

--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -3,10 +3,11 @@ import { InfrastructureModule } from './infrastructure';
 import { CqrsModule } from '@nestjs/cqrs';
 import * as EventHandlers from './application/event-handlers';
 import { InfrastructureModule as AccountsInfrastructureModule } from '../accounts/infrastructure';
+import { SearchService } from './application';
 
 @Module({
   imports: [InfrastructureModule, CqrsModule, AccountsInfrastructureModule],
-  providers: [...Object.values(EventHandlers)],
-  exports: [],
+  providers: [...Object.values(EventHandlers), SearchService],
+  exports: [SearchService],
 })
 export class SearchModule {}
