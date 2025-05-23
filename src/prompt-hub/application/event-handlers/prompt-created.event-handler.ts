53080f42f62b769069af4cd31f2c8e66d59439a4
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-05-23 12:48:02 +0200

    Add PromptCreatedEvent and handling infrastructure
    Introduce `PromptCreatedEvent` to capture the creation of prompts and its associated event handler for managing prompt views. This ensures prompt creations are reflected properly in both list and detailed views, supporting consistency across repositories.

---
+++ src/prompt-hub/application/event-handlers/prompt-created.event-handler.ts
@@ -0,0 +1,113 @@
+import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
+import {
+  PromptContent,
+  PromptCreatedEvent,
+  PromptId,
+  PromptStatus,
+  PromptTimestamps,
+  PromptTitle,
+  PromptVisibility,
+} from '../../domain';
+import {
+  PromptDetailsViewRepository,
+  PromptListItemViewRepository,
+  PromptUserPublicRepository,
+} from '../ports';
+import {
+  PromptDetailsView,
+  PromptListItemView,
+  PromptUserPublicView,
+} from '../../views';
+
+@EventsHandler(PromptCreatedEvent)
+export class PromptCreatedEventHandler
+  implements IEventHandler<PromptCreatedEvent>
+{
+  constructor(
+    private readonly promptListItemViewRepository: PromptListItemViewRepository,
+    private readonly promptUserPublicRepository: PromptUserPublicRepository,
+    private readonly promptDetailsViewRepository: PromptDetailsViewRepository,
+  ) {}
+
+  async handle(event: PromptCreatedEvent) {
+    const {
+      promptId,
+      content,
+      authorId,
+      title,
+      timestamps,
+      status,
+      visibility,
+    } = event;
+
+    const author = await this.promptUserPublicRepository.findById(
+      authorId.getValue(),
+    );
+
+    if (!author) return;
+
+    await this.createPromptListItemView(
+      promptId,
+      title,
+      content,
+      timestamps,
+      author,
+    );
+
+    await this.createPromptDetailsView(
+      promptId,
+      title,
+      content,
+      timestamps,
+      status,
+      visibility,
+      author,
+    );
+  }
+
+  private async createPromptListItemView(
+    promptId: PromptId,
+    title: PromptTitle,
+    content: PromptContent,
+    timestamps: PromptTimestamps,
+    author: PromptUserPublicView,
+  ) {
+    const promptListItemView = new PromptListItemView(
+      promptId.getValue(),
+      title.getValue(),
+      content.getPreview(),
+      0, // likedCount
+      0, // copiedCount
+      0, // viewCount
+      timestamps.getCreatedAt(),
+      author,
+    );
+
+    await this.promptListItemViewRepository.save(promptListItemView);
+  }
+
+  private async createPromptDetailsView(
+    promptId: PromptId,
+    title: PromptTitle,
+    content: PromptContent,
+    timestamps: PromptTimestamps,
+    status: PromptStatus,
+    visibility: PromptVisibility,
+    author: PromptUserPublicView,
+  ) {
+    const promptDetailsView = new PromptDetailsView(
+      promptId.getValue(),
+      title.getValue(),
+      content.getValue(),
+      visibility.isPublic(),
+      status.getValue(),
+      timestamps.getCreatedAt(),
+      0, // likedCount
+      0, // copiedCount
+      0, // viewCount
+      author,
+    );
+
+    await this.promptDetailsViewRepository.save(promptDetailsView);
+  }
+}
---
+++ src/prompt-hub/domain/events/prompt-created.event.ts
@@ -0,0 +1,22 @@
+import { IEvent } from '@nestjs/cqrs';
+import {
+  PromptContent,
+  PromptId,
+  PromptStatus,
+  PromptTimestamps,
+  PromptTitle,
+  PromptVisibility,
+  UserId,
+} from '../value-objects';
+
+export class PromptCreatedEvent implements IEvent {
+  constructor(
+    public readonly promptId: PromptId,
+    public readonly authorId: UserId,
+    public readonly title: PromptTitle,
+    public readonly content: PromptContent,
+    public readonly status: PromptStatus,
+    public readonly visibility: PromptVisibility,
+    public readonly timestamps: PromptTimestamps,
+  ) {}
+}
