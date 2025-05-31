6471df13840cccdd03a06fea205712e4b45f383e
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-05-31 13:27:59 +0200

    Add repositories for prompt statistics and daily stats
    Introduced abstract repositories `PromptStatisticsViewRepository` and `PromptDailyStatsViewRepository` to manage prompt-related statistics and daily stats. Updated the index file to export these repositories for external usage.

--- src/statistics/application/ports/index.ts
+++ src/statistics/application/ports/index.ts
@@ -1,1 +1,2 @@
-export {};
+export * from './prompt-statistics-view.repository';
+export * from './prompt-daily-stats-view.repository';
---
+++ src/statistics/application/ports/prompt-daily-stats-view.repository.ts
@@ -0,0 +1,11 @@
+import { PromptDailyStatsView } from '../../views';
+
+export abstract class PromptDailyStatsViewRepository {
+  abstract save(promptDailyStatsView: PromptDailyStatsView): Promise<void>;
+  abstract findById(id: string): Promise<PromptDailyStatsView>;
+  abstract incrementViewCount(id: string): Promise<void>;
+  abstract incrementLikedCount(id: string): Promise<void>;
+  abstract incrementDislikedCount(id: string): Promise<void>;
+  abstract incrementFavoritesCount(id: string): Promise<void>;
+  abstract incrementCopiedCount(id: string): Promise<void>;
+}
